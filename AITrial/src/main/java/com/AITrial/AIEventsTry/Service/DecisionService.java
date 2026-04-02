a42ea5088a08c777ecb139ba6697e753474f0961
Rounaak Chaudhari <125425715+CJ-0-11-73672@users.noreply.github.com>
2026-04-02 15:56:29 +0530

    Update DecisionService.java

--- AITrial/src/main/java/com/AITrial/AIEventsTry/Service/DecisionService.java
+++ AITrial/src/main/java/com/AITrial/AIEventsTry/Service/DecisionService.java
@@ -3,7 +3,7 @@ package com.AITrial.AIEventsTry.Service;
 
 import com.AITrial.AIEventsTry.Entities.AIEntities;
 import com.AITrial.AIEventsTry.Entities.AIResponse;
-
+import java.util.Map;
 public interface DecisionService {
   Map<String, Object> evaluate(Map<String, Object> request);
 }
