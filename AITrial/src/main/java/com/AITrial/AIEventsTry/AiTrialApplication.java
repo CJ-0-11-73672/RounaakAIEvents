b19ca8583d75500848555bd0bff8fa13b66e19c7
Rounaak Chaudhari <125425715+CJ-0-11-73672@users.noreply.github.com>
2026-04-05 17:29:47 +0530

    Update AiTrialApplication.java

--- AITrial/src/main/java/com/AITrial/AIEventsTry/AiTrialApplication.java
+++ AITrial/src/main/java/com/AITrial/AIEventsTry/AiTrialApplication.java
@@ -2,8 +2,10 @@ package com.AITrial.AIEventsTry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class AiTrialApplication {
 
 	public static void main(String[] args) {
