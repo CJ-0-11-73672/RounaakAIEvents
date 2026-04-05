983f9c3416c1e03cac75373a956879dd308af903
Rounaak Chaudhari <125425715+CJ-0-11-73672@users.noreply.github.com>
2026-04-05 17:32:20 +0530

    Update AppConfig.java

--- AITrial/src/main/java/com/AITrial/AIEventsTry/AppConfig/AppConfig.java
+++ AITrial/src/main/java/com/AITrial/AIEventsTry/AppConfig/AppConfig.java
@@ -2,12 +2,24 @@ package com.AITrial.AIEventsTry.AppConfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
+import java.util.concurrent.Executor;
 
 @Configuration
 public class AppConfig {
 @Bean
 public RestTemplate restTemplate(){
     return new RestTemplate();
+}
+@Bean("taskExecutor")
+public Executor taskExecutor(){
+ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+executor.setCorePoolSize(10);
+executor.setMaxPoolSize(50);
+executor.setQueueCapacity(100);
+executor.setThreadNamePrefix("AI-Thread");
+executor.initialize();
+return executor;
 }
 }
